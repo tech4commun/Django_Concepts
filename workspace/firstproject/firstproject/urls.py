c017ddb6bccfcd150e985e2ee7f33e57263a3926
tech4commun <pedaltriangle@gmail.com>
2026-04-20 00:31:29 +0530

    Add URL pattern for greetings view in urls.py

--- workspace/firstproject/firstproject/urls.py
+++ workspace/firstproject/firstproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from testapp  import views
 
 urlpatterns = [
+    
+    path("Hello", views.greetings),
     path("admin/", admin.site.urls),
 ]
